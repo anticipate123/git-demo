073829aa0c628f97609f3a004387ddac99073cb4
zengxin <zengxin@qq.com>
2020-01-17 12:25:10 +0800

    我只添加了源文件-画出菱形.c

---
+++ 画出菱形.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	int i, j, n;
+	printf("Please input a integer:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=1;j<n-i;j++)
+		printf(" ");
+		for(j=0;j<2*i+1;j++)
+		printf("*");
+		printf("\n");
+	}
+	for(i=n-2;i>=0;i--)
+	{
+		for(j=1;j<n-i;j++)
+		printf(" ");
+		for(j=0;j<2*i+1;j++)
+		printf("*");
+		printf("\n");
+	}
+/*	
+	for(i=1;i<n;i++)
+	{
+		for(j=0;j<i;j++)
+		printf(" ");
+		for(j=0;j<2*n-1-2*i;j++)
+		printf("*");
+		printf("\n");
+	}
+*/
+//	system("pause");
+}
