66dc596bdf74e5c281b8df9fbcd050d260372d08
zengxin <zengxin@qq.com>
2020-01-17 12:44:27 +0800

    完善得更好了

--- 画出菱形.c
+++ 画出菱形.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include<stdio.h>//做得更好了 
 #include<windows.h>
 int main()
 {
 	int i, j, n;
 	printf("Please input a integer:");
-	scanf("%d",&n);
+	scanf("%d",&n);{//添加了注释 
 	for(i=0;i<n;i++)
-	{
+	{//添加了注释 
 		for(j=1;j<n-i;j++)
 		printf(" ");
-		for(j=0;j<2*i+1;j++)
-		printf("*");
+		for(j=0;j<2*i+1;j++){//添加了注释 
+		printf("*");{//添加了注释 
 		printf("\n");
 	}
-	for(i=n-2;i>=0;i--)
+	for(i=n-2;i>=0;i--){//添加了注释 
 	{
 		for(j=1;j<n-i;j++)
 		printf(" ");
